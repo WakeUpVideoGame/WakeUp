619cc18c88c9ebc2479ad3914d73b5a6aea88a65
AdrienMgm <adrien.mgm@gmail.com>
2015-03-24 17:49:33 +0100

    UP: try other configuration to leap

--- Source/WakeUp/Private/WakeUpLeapController.cpp
+++ Source/WakeUp/Private/WakeUpLeapController.cpp
@@ -25,9 +25,12 @@ AWakeUpLeapController::AWakeUpLeapController(const FObjectInitializer& ObjectIni
 	{
 		// Enable some gestures
 		Device->getLeapController().enableGesture(Leap::Gesture::TYPE_SWIPE);
+		Device->getLeapController().enableGesture(Leap::Gesture::TYPE_CIRCLE);
 
-		Device->getLeapController().config().setFloat("Gesture.Swipe.MinLength", 100.0);
+		Device->getLeapController().config().setFloat("Gesture.Swipe.MinLength", 150.0);
 		Device->getLeapController().config().setFloat("Gesture.Swipe.MinVelocity", 50);
+		Device->getLeapController().config().setFloat("Gesture.Circle.MinRadius", 100.0);
+		Device->getLeapController().config().setFloat("Gesture.Circle.MinArc", 5.);
 		Device->getLeapController().config().save();
 	}
 }
@@ -40,81 +43,82 @@ void AWakeUpLeapController::Tick(float DeltaTime)
 
 	FLeapMotionDevice* Device = FLeapMotionControllerPlugin::GetLeapDeviceSafe();
 
-	if (Wait > 0) --Wait;
-
-	if (Device && Device->IsConnected())
+	if (Wait <= 0)
 	{
-		Device->SetReferenceFrameOncePerTick();
-		Leap::Frame frame = Device->Frame();
+		Wait = 0;
+		if (Device && Device->IsConnected())
+		{
+			Device->SetReferenceFrameOncePerTick();
+			Leap::Frame frame = Device->Frame();
 
-// 		for (Leap::Hand hand : frame.hands())
-// 		{
-// 			for (Leap::Finger finger : hand.fingers()){
-// 				FString fingerLabel = FString::Printf(TEXT("        Finger %u: "), finger.id());
-// 				fingerLabel += finger.tipPosition().toString().c_str();
-// 				GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Green, fingerLabel);
-// 			}
-// 
-// 			FString handLabel = FString::Printf(TEXT("    Hand %u: "), hand.id());
-// 			handLabel += hand.palmPosition().toString().c_str();
-// 			GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Green, handLabel);
-// 		}
+			// 		for (Leap::Hand hand : frame.hands())
+			// 		{
+			// 			for (Leap::Finger finger : hand.fingers()){
+			// 				FString fingerLabel = FString::Printf(TEXT("        Finger %u: "), finger.id());
+			// 				fingerLabel += finger.tipPosition().toString().c_str();
+			// 				GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Green, fingerLabel);
+			// 			}
+			// 
+			// 			FString handLabel = FString::Printf(TEXT("    Hand %u: "), hand.id());
+			// 			handLabel += hand.palmPosition().toString().c_str();
+			// 			GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Green, handLabel);
+			// 		}
 
-// 		FString leapLabel = FString::Printf(TEXT("LeapController - Frame: %u"), frame.id());
-// 		GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Green, leapLabel);
+			// 		FString leapLabel = FString::Printf(TEXT("LeapController - Frame: %u"), frame.id());
+			// 		GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Green, leapLabel);
 
-		// Handle gesture
-		if (frame.isValid() && frame.gestures().count() > 0) {
-			for (Leap::Gesture gest : frame.gestures()) {
-				Leap::SwipeGesture swipeGesture = Leap::Gesture::invalid();
-				if (gest.type() == Leap::Gesture::TYPE_SWIPE) {
-					swipeGesture = Leap::SwipeGesture(gest);
+			// Handle gesture
+			if (frame.isValid() && frame.gestures().count() > 0) {
+				for (Leap::Gesture gest : frame.gestures()) {
 
-					GEngine->AddOnScreenDebugMessage(-1, 1000000.f, FColor::Cyan, FString(TEXT("It's a swipe")));
+					if (gest.type() == Leap::Gesture::TYPE_CIRCLE){
+						Leap::CircleGesture circleGesture = Leap::CircleGesture(gest);
+						GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Yellow, FString(TEXT("Action, it's a circle !")));
+					}
 
-					// Classify swipe as either horizontal or vertical
-					auto isHorizontal = abs(swipeGesture.direction().x) > abs(swipeGesture.direction().y);
-					// Classify as right-left or up-down
-					if (isHorizontal){
-						if (swipeGesture.direction().x > 0){
-							GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Cyan, FString(TEXT("Right")));
-							if (LPLeft)
-							{
+					if (gest.type() == Leap::Gesture::TYPE_SWIPE) {
+						Leap::SwipeGesture swipeGesture = Leap::SwipeGesture(gest);
+
+						// Classify swipe as either horizontal or vertical
+						auto isHorizontal = abs(swipeGesture.direction().x) > abs(swipeGesture.direction().y);
+						// Classify as right-left or up-down
+						if (isHorizontal){
+							if (swipeGesture.direction().x > 0){
+								GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Cyan, FString(TEXT("Right")));
 								LPLeft = false;
-							}
-							else
-							{
 								LPRight = true;
+								MyCharacter->CharacterMovement->MaxWalkSpeed = swipeGesture.speed() * 2;
+							}
+							else {
+								GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Yellow, FString(TEXT("Left")));
+								LPRight = false;
+								LPLeft = true;
+								MyCharacter->CharacterMovement->MaxWalkSpeed = swipeGesture.speed() * 2;
 							}
 						}
-						else {
-							GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Yellow, FString(TEXT("Left")));
-							if (LPRight)
-							{
+						else { //vertical
+							if (swipeGesture.direction().y > 0){
+								GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Black, FString(TEXT("Up")));
+								Jump();
+							}
+							else {
+								GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::White, FString(TEXT("Down")));
+								LPLeft = false;
 								LPRight = false;
 							}
-							else
-							{
-								LPLeft = true;
-							}
-						}
-					}
-					else { //vertical
-						if (swipeGesture.direction().y > 0){
-							GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::Black, FString(TEXT("Up")));
-							Jump();
-						}
-						else {
-							GEngine->AddOnScreenDebugMessage(-1, 1000.f, FColor::White, FString(TEXT("Down")));
 						}
 					}
 				}
 			}
 		}
+		else{
+			GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Green, FString(TEXT("LeapController - No Data")));
+			/*UE_LOG(Controller, Warning, TEXT("WakeUpLeapController::LeapController - No Data"));*/
+		}
 	}
-	else{
-		GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Green, FString(TEXT("LeapController - No Data")));
-		/*UE_LOG(Controller, Warning, TEXT("WakeUpLeapController::LeapController - No Data"));*/
+	else
+	{
+		--Wait;
 	}
 
 	if (LPLeft) MoveRight(-1.f);
